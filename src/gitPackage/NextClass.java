bdf2bbe20347d76516267db78f735fae91418240
mahmutsalik <62161817+mahmutsalik@users.noreply.github.com>
2020-03-14 16:58:52 -0400

    Commit 10

--- src/gitPackage/NextClass.java
+++ src/gitPackage/NextClass.java
@@ -1,11 +1,16 @@
 package gitPackage;
-
+import java.util.Scanner;
 public class NextClass {
 
  public static void main(String[] args) {
 	 
 	 
 	 System.out.println("Hello dear Testers");
+	 Scanner scan = new Scanner(Sytem.in);
+	 System.out.prinln("Enter Name");
+	 String name = scan.next();
+	 System.out.println(name);
+	 
 } 
  
                      
