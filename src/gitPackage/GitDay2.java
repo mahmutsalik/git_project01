36fe95fefb292c5376a0f6a8b999a3ece161d46c
Mahmut <mahmutsalik@gmail.com>
2020-03-14 16:17:25 -0400

    commit 7

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -9,7 +9,9 @@ public class GitDay2 {
 	
 	System.out.println("Hello world!");
 	System.out.println("Hello Git Users");
-	System.out.println("Check if your local is ahead of origin master");	
+	System.out.println("Check if your local is ahead of origin master");
+	System.out.println("commit 7");
+	
 	}
 	
 	
